9ac5fae519fbbe20296177008fb4d7d63e7f83a0
PLS54 <32674866+PLS54@users.noreply.github.com>
2017-10-10 17:00:20 -0400

    New Examples
    Added libraries examples

---
+++ Libraries/Potientiometer/Examples/BlinkTest/BlinkTest.ino
@@ -0,0 +1,104 @@
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+
+#include "Potentiometer.h"
+
+float oldVoltage = -1.0;
+int counter = 1;
+Potentiometer pot(A2, 1, 10);
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Blick tester");
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+  //Serial.println("Start of loop");
+  //float voltage = pot.GetValue(0.005);
+  //if (voltage  != oldVoltage) {
+//    // print out the value you read:
+//    char tst[50];
+//    sprintf(tst, "%d - value %d.%d", counter++, (int)voltage, (int)(voltage * 1000.0) % 1000); 
+//    Serial.println(tst);
+//    oldVoltage = voltage;
+//    if (Potentiometer::InBetween(voltage, 4.99, 5.0f))
+//    {
+//    }
+//  }
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(10);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+//  pinMode(LED_BUILTIN, INPUT);
+  delay(990);                       // wait for a second
+}
+
+float roundNearest(float value, float decimal)
+{
+  return round(value * (1.0 / decimal)) / (1.0 / decimal);
+}
+
+void flashSOS()
+{
+  flashS();
+  flashO();
+  flashS();
+}
+void flashS()
+{
+  shortDash();
+  shortDash();
+  shortDash();
+}
+void flashO()
+{
+  longDash();
+  longDash();
+  longDash();
+}
+#define DELAY 500
+#define LONG_DASH 500
+#define SHORT_DASH LONG_DASH / 2
+
+void longDash()
+{
+  if (true) return;
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(LONG_DASH);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(DELAY);                       // wait for a second
+}
+
+void shortDash()
+{
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(SHORT_DASH);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(DELAY);                       // wait for a second
+}
+
---
+++ Libraries/Timer/Examples/TimerTest/TimerTest.ino
@@ -0,0 +1,113 @@
+#include <Timer.h>
+
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+
+#define TRIG_PIN 11
+#define ECHO_PIN 10
+#define DOOR_PIN 9
+
+bool triggerDone = false;
+Timer myTimer;
+Timer sentinel(60000);
+bool doorMoving = false;
+void setup() 
+{
+  Serial.begin (9600);
+  for (byte i = 0; i <= A5; i++) {
+    pinMode (i, OUTPUT);    // changed as per below
+    digitalWrite (i, LOW);  //     ditto
+  }
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(DOOR_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(DOOR_PIN, HIGH);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  Serial.println("Setup complete");
+}
+
+void loop() 
+{
+  long distance = MeasureDistance(TRIG_PIN, ECHO_PIN);
+  if (distance <= 10.0) {
+    if (!triggerDone) {
+      pulse();
+      myTimer.Start(15000);
+      triggerDone = true;
+      doorMoving = true;
+    }
+  } else {
+    triggerDone = false;
+  } 
+  if (myTimer.IsElapse()) {
+    if (doorMoving) {
+      Serial.println("");
+    }
+    doorMoving = false;
+  } else {
+    Serial.print(".");
+    delay(500); // do not send report too often
+  }
+  if (sentinel.IsElapse()) {
+    Serial.print("T");
+  }
+  delay(500);
+}
+
+
+void pulse()
+{
+  digitalWrite(DOOR_PIN, LOW);
+  digitalWrite(LED_BUILTIN, HIGH);
+  for (int i = 0; i < 10; i++) {
+    delayMicroseconds(10000);    
+  }
+  digitalWrite(DOOR_PIN, HIGH);   
+  digitalWrite(LED_BUILTIN, LOW); 
+}
+
+long MeasureDistance(int trigPin, int echoPin)
+{
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  return (pulseIn(echoPin, HIGH) / 58.2);
+} 
+
+// watchdog interrupt
+ISR (WDT_vect) 
+{
+   wdt_disable();  // disable watchdog
+}  // end of WDT_vect
+
+void loseOneSecond()
+{
+  // disable ADC
+ADCSRA = 0;  
+
+// clear various "reset" flags
+MCUSR = 0;     
+// allow changes, disable reset
+WDTCSR = bit (WDCE) | bit (WDE);
+// set interrupt mode and an interval 
+WDTCSR = bit (WDIE) | bit (WDP2) | bit (WDP1);    // set WDIE, and 1 second delay
+wdt_reset();  // pat the dog
+
+set_sleep_mode (SLEEP_MODE_PWR_DOWN);  
+noInterrupts ();           // timed sequence follows
+sleep_enable();
+
+// turn off brown-out enable in software
+MCUCR = bit (BODS) | bit (BODSE);
+MCUCR = bit (BODS); 
+interrupts ();             // guarantees next instruction executed
+sleep_cpu ();  
+
+// cancel sleep as a precaution
+sleep_disable();
+
+}
+
