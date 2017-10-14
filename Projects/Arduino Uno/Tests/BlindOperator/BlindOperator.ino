642dc2fb6972d3272d685763fbe8bf0b5560157f
PLS54 <plsigouin@gmail.com>
2017-10-14 15:40:37 -0400

    WIP
    Added a IR remote disable function

--- Projects/Arduino Uno/Tests/BlindOperator/BlindOperator.ino
+++ Projects/Arduino Uno/Tests/BlindOperator/BlindOperator.ino
@@ -1,6 +1,7 @@
 #include <IRremote.h>
 #include <Stepper.h>
 #include "Blind.h"
+#include "Timer.h"
 
 #define DOOR_PIN 9
 #define DELAY 250
@@ -16,6 +17,9 @@
 #define LEFT_ARROW      0x5E108
 #define RIGHT_ARROW     0xDE108
 #define RETURN          0xBE108
+#define YELLOW          0xE010C
+#define BLUE            0x2010C
+#define ENTER           0x3E108
 
 #define MOVE_TO_0       FAST_BACKWARD
 #define MOVE_TO_50      PLAY
@@ -32,7 +36,9 @@ Stepper blindMotor(STEPS_PER_MOTOR_REVOLUTION, 8, 10, 9, 11);
 Blind myBlind(&blindMotor, 400);
 IRrecv irDetect(IR_PIN);
 decode_results irIn;
+Timer remoteInactive;
 unsigned long lastCommand = 0;
+bool remoteActive = true;
 
 void setup()
 {
@@ -46,29 +52,41 @@ void loop() {
   if (irDetect.decode(&irIn)) {
 //    Serial.println(irIn.value, HEX);
     irDetect.resume(); // Receive the next value
+    if (remoteInactive.IsElapse()) {
+      remoteActive = true;
+    }
+    if (irIn.value == ENTER && lastCommand == YELLOW) {
+      remoteActive = true;
+    }
+    if (irIn.value == ENTER && lastCommand == BLUE) {
+      remoteInactive.Start(10000);
+      remoteActive = false;
+    }
     if (irIn.value != REPEAT) {
       lastCommand = irIn.value;
     }
-    if (irIn.value == ADJUST_DOWN || (irIn.value == REPEAT && lastCommand == ADJUST_DOWN)) {
-      blindMotor.step(-1);
-    }
-    if (irIn.value == ADJUST_UP || (irIn.value == REPEAT && lastCommand == ADJUST_UP)) {
-      blindMotor.step(1);
-    }
-    if (irIn.value == MOVE_DOWN_10 || (irIn.value == REPEAT && lastCommand == MOVE_DOWN_10)) {
-      myBlind.Move(-10);
-    }
-    if (irIn.value == MOVE_UP_10 || (irIn.value == REPEAT && lastCommand == MOVE_UP_10)) {
-      myBlind.Move(10);
-    }
-    if (irIn.value == MOVE_TO_0) {
-      myBlind.MoveTo(0);
-    }
-    if (irIn.value == PLAY) {
-      myBlind.MoveTo(50);
-    }
-    if (irIn.value == MOVE_TO_100) {
-      myBlind.MoveTo(100);
+    if (remoteActive) {
+      if (irIn.value == ADJUST_DOWN || (irIn.value == REPEAT && lastCommand == ADJUST_DOWN)) {
+        blindMotor.step(-1);
+      }
+      if (irIn.value == ADJUST_UP || (irIn.value == REPEAT && lastCommand == ADJUST_UP)) {
+        blindMotor.step(1);
+      }
+      if (irIn.value == MOVE_DOWN_10 || (irIn.value == REPEAT && lastCommand == MOVE_DOWN_10)) {
+        myBlind.Move(-10);
+      }
+      if (irIn.value == MOVE_UP_10 || (irIn.value == REPEAT && lastCommand == MOVE_UP_10)) {
+        myBlind.Move(10);
+      }
+      if (irIn.value == MOVE_TO_0) {
+        myBlind.MoveTo(0);
+      }
+      if (irIn.value == PLAY) {
+        myBlind.MoveTo(50);
+      }
+      if (irIn.value == MOVE_TO_100) {
+        myBlind.MoveTo(100);
+      }
     }
     if (irIn.value == REPORT_POSITION) {
       unsigned int pos = myBlind.GetCurrentPosition();
