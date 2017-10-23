226b4fecd9843826036a87825b25af7c29374fb2
PLS54 <plsigouin@gmail.com>
2017-10-23 13:03:45 -0400

    Added a real decode tester to SonyIRDeocder

---
+++ Libraries/SonyIRRemoteDecoder/Examples/RealDecode/RealDecode.ino
@@ -0,0 +1,25 @@
+#include <IRremote.h>
+#include <SonyIRRemoteDecoder.h>
+
+int recvPin = 13;
+IRrecv irrecv(recvPin);
+SonyIRRemoteDecoder decoder;
+
+void setup() 
+{
+  irrecv.enableIRIn();  // Start the receiver
+  Serial.begin(9600);   // Status message will be sent to PC at 9600 baud
+}
+
+void loop() 
+{
+  decode_results  results;        // Somewhere to store the results
+  if (irrecv.decode(&results)) {
+    if (decoder.DecodeRaw(results.rawbuf, results.rawlen)) {
+      Serial.print("Result: 0x");
+      Serial.println(decoder.GetValue(), HEX);
+    }
+    irrecv.resume();              // Prepare for the next value
+  }
+}
+
